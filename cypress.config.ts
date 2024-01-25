87747455ff404906752f29edb4be264f8993ceab
programmer-fe3 <programmer-fe3@qwertysystem.net>
2024-01-25 09:04:38 +0700

    tests(cypress): add baseUrl for the test host

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,14 +3,15 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   projectId: 'b2yz2p',
   e2e: {
+    baseUrl: 'http://localhost:5173',
     specPattern: 'cypress/e2e/**/*.cy.spec.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173',
     supportFile: false,
   },
   component: {
     specPattern: 'src/**/*.cy.spec.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/component.ts',
     devServer: {
+      baseUrl: 'http://localhost:5173',
       framework: 'vue',
       bundler: 'vite',
     },
