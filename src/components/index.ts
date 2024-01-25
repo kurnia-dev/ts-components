f2bb58b2f856cfcf9b367adf820e670ad29b7982
programmer-fe3 <programmer-fe3@qwertysystem.net>
2024-01-25 16:30:01 +0700

    chore: updae ts config to ignore testt file and cypress

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,16 @@
 import type { App } from 'vue';
 import TSButton from './button/TSButton.vue';
 import TSInputText from './lib/inputtext/TSInputText.vue';
+import SelectGroup from './lib/selectgroup/SelectGroup.vue';
+import GroupTree from './lib/grouptree/GroupTree.vue';
 
 export default {
   install: (app: App): void => {
     app.component('TSButton', TSButton);
     app.component('TSInputText', TSInputText);
+    app.component('SelectGroup', SelectGroup);
+    app.component('GroupTree', GroupTree);
   },
 };
 
-export { TSButton, TSInputText };
+export { TSButton, TSInputText, SelectGroup, GroupTree };
