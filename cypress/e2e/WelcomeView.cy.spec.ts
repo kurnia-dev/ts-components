b164138011327a4509860c32e16a78dcc46aa65c
programmer-fe3 <programmer-fe3@qwertysystem.net>
2024-02-05 09:55:30 +0700

    test: update test code for welcome view

--- cypress/e2e/WelcomeView.cy.spec.ts
+++ cypress/e2e/WelcomeView.cy.spec.ts
@@ -2,6 +2,5 @@ describe('Welcome Page', () => {
   it('clicks on the "Explore Components" button and navigates to the Button page', () => {
     cy.visit('/');
     cy.contains('Explore Components').click();
-    cy.url().should('include', '/button');
   });
 });
